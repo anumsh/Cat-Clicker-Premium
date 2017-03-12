735ecbbc3be136092b73eab281bdc32574bbb9b7
anumsh <anumsharma09@gmail.com>
2017-03-11 19:05:54 -0800

    feat:add new javascript file and knouknot file

---
+++ knockout/cat-clicker-knockout/js/app.js
@@ -0,0 +1,38 @@
+var Cat=function() {
+    this.clickCount= ko.observable(0);
+    this.name= ko.observable("Tabby");
+    this.imageSrc= ko.observable("img/cat1.jpg");
+    this.nickNames=ko.observableArray(['catty1','catty2','catty3','catty4','catty5']);
+
+    this.title=ko.computed(function(){
+      var title;
+      var click=this.clickCount();
+      if(click < 10){
+        title="newborn";
+      }
+      else if(click < 50){
+        title="infant";
+      }
+      else if(click < 100){
+        title="child";
+      }
+      else if(click < 200){
+        title="teen";
+      }
+      else if(click < 500){
+        title="ninja";
+      }
+      return title;
+    }, this);
+};
+
+
+var ViewModel=function() {
+  this.currentCat= ko.observable(new Cat());
+  this.incrementCount=function() {
+    return this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
+};
+
+
+ko.applyBindings(new ViewModel());
